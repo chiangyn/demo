df878f464962af4d6b745fc3b305ea1acbe414ab
chiang <chiangyaniu@163.com>
2014-05-30 10:03:02 +0800

    包重命名

---
+++ hello/src/com/ycfy/weixin/App.java
@@ -0,0 +1,10 @@
+package com.ycfy.weixin;
+
+public class App {
+
+	public static void main(String[] args) {
+       System.out.println("Hello Git !");
+       System.out.println("Hello World!");
+	}
+
+}
