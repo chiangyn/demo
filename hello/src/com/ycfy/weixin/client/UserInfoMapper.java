5a48934520d41d0bc576653a9f7b2d7d63b3e0c2
chiang <chiangyaniu@163.com>
2014-06-05 11:16:46 +0800

    添加映射

---
+++ hello/src/com/ycfy/weixin/client/UserInfoMapper.java
@@ -0,0 +1,17 @@
+package com.ycfy.weixin.client;
+
+import com.ycfy.weixin.model.UserInfo;
+
+public interface UserInfoMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(UserInfo record);
+
+    int insertSelective(UserInfo record);
+
+    UserInfo selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(UserInfo record);
+
+    int updateByPrimaryKey(UserInfo record);
+}
---
+++ hello/src/com/ycfy/weixin/model/UserInfo.java
@@ -0,0 +1,37 @@
+package com.ycfy.weixin.model;
+
+import java.io.Serializable;
+
+public class UserInfo implements Serializable {
+    private Integer id;
+
+    private String nick;
+
+    private String pwd;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick == null ? null : nick.trim();
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd == null ? null : pwd.trim();
+    }
+}
